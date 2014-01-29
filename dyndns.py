b473916ac74497bfc315718da8a18f001aa06898
zwerch <zwerch1337@googlemail.com>
2014-01-29 02:00:28 +0100

    added request until we got the damn id

--- dyndns.py
+++ dyndns.py
@@ -19,12 +19,27 @@ def readconfig():
     domain = cfg.get("General", "domain")
 
 def main():
+    # login credentials
     cred = {"lang": "en", "user": username, "pass": password}
+    # domain for request
     dom = {"domain": domain}
+    # domrobot object (for request)
     conn = inwx.domrobot(api_url)
+    # login
     login = conn.account.login(cred)
-    check = conn.domain.check(dom)
-    print(inwx.prettyprint.domain_check(check))
+    # get nameserver entries
+    ninfo = conn.nameserver.info(dom)
+    ncount = 0
+    # get the one with "server."
+    for i in range(len(ninfo["record"])):
+        if ninfo["record"][i]["name"] == ("server." + domain):
+            ncount = i
+            break
+    # save the id of the entry
+    nid = ninfo["record"][ncount]["id"]
+
+    # TODO
+    # update the record
 
 if __name__ == "__main__":
     readconfig()
